3e82278308e6cd52726f924aa5438ebb75508e97
Armando Cordova <armanpela@gmail.com>
2019-03-18 18:50:42 -0500

    Use jest to test, configure ts and html files in tests

--- test/test.test.ts
+++ test/test.test.ts
@@ -1,10 +1,7 @@
-import "mocha";
-import { expect } from "chai";
-
 describe("Array", function() {
   describe("#indexOf()", function() {
     it("should return -1 when the value is not present", function() {
-      expect([1, 2, 3].indexOf(4)).eq(-1);
+      expect([1, 2, 3].indexOf(4)).toBe(-1);
     });
   });
 });
---
+++ test/todos.test.ts
@@ -0,0 +1,47 @@
+import { createConfig } from "../src/overko";
+import { createOverkoMock } from "overko";
+import Overtodo from "../src/viewmodels/overtodo";
+
+describe("Todos", () => {
+  describe("initialization", () => {
+    it("should add items to the todo lists on Initialization", async () => {
+      const overko = createOverkoMock(createConfig(), {
+        todosCache: {
+          getTodos: () => [{ title: "title" }],
+          setTodos: () => {}
+        }
+      });
+
+      const lengthBefore = overko.state.todos.list().length;
+      expect(lengthBefore).toBe(0);
+
+      overko.onInitialize();
+      await overko.initialized;
+
+      const lengthAfter = overko.state.todos.list().length;
+      expect(lengthAfter).toBe(1);
+    });
+  });
+
+  describe("todo viewModel", () => {
+    it("should add items to the todo lists on Initialization", async () => {
+      const mock = createOverkoMock(createConfig(), {
+        todosCache: {
+          getTodos: () => [{ title: "title" }],
+          setTodos: () => {}
+        }
+      });
+
+      const todo = new Overtodo({ overko: mock });
+
+      const lengthBefore = todo.todos().length;
+      expect(lengthBefore).toBe(0);
+
+      mock.onInitialize();
+      await mock.initialized;
+
+      const lengthAfter = todo.todos().length;
+      expect(lengthAfter).toBe(1);
+    });
+  });
+});
