f3947cdd4d2deedbd9e973052203c21d42a70299
Leonel <locsa@belatrixsf.com>
2019-03-21 16:46:01 -0500

    New Design
    A new design has been done to show the grid data

--- src/overko/state.ts
+++ src/overko/state.ts
@@ -11,12 +11,12 @@ const state = {
   },
   adsetlist: {
     data: [
-      { data1: "Data 1", data2: "Data 2", data3: "Data 3", data4: "Data 4" },
-      { data1: "Data 1", data2: "Data 2", data3: "Data 3", data4: "Data 4" },
-      { data1: "Data 1", data2: "Data 2", data3: "Data 3", data4: "Data 4" },
-      { data1: "Data 1", data2: "Data 2", data3: "Data 3", data4: "Data 4" }
-      { data1: "Data 1", data2: "Data 2", data3: "Data 3", data4: "Data 4" },
-      { data1: "Data 1", data2: "Data 2", data3: "Data 3", data4: "Data 4" }
+      { data1: "The Grasslands", data2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet.", data3: "Data 3", data4: "Data 4" },
+      { data1: "Paradise Found", data2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet.", data3: "Data 3", data4: "Data 4" },
+      { data1: "Smoke On The Water", data2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet.", data3: "Data 3", data4: "Data 4" },
+      { data1: "Headline", data2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet.", data3: "Data 3", data4: "Data 4" }
+      { data1: "Hello World", data2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet.", data3: "Data 3", data4: "Data 4" },
+      { data1: "Enter Sandman", data2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet.", data3: "Data 3", data4: "Data 4" }
     ]
   }
 };
