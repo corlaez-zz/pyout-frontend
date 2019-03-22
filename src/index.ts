eadf75317ce504851486b06e43e96d8ffaa79244
Leonel <locsa@belatrixsf.com>
2019-03-22 15:37:02 -0500

    Genderpicker added to main view

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as ko from "knockout";
 import { todoViewModelRegister } from "./viewmodels/todo";
 import { registerOvertodoViewModel } from "./viewmodels/overtodo";
+import { registerGenderPickerViewModel } from "./viewmodels/genderpicker";
 import { registerPlatformPickerViewModel } from "./viewmodels/platform";
 import "./extends/handlers";
 import "./styles/base.css";
@@ -9,6 +10,7 @@ import { getTime } from "./overko/effects/api";
 
 todoViewModelRegister();
 registerOvertodoViewModel();
+registerGenderPickerViewModel();
 registerPlatformPickerViewModel();
 
 const App = () => {};
